49053836fe2a98f5e05c1a3017c30acee7eecae9
hanbee <sonhanbi1002@gmail.com>
2021-04-02 23:37:10 +0900

    네트워크 문제 dfs 로 수정하기

--- Programmers/PG_네트워크/Network.java
+++ Programmers/PG_네트워크/Network.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 public class Network {
     public static void main(String[] args) {
         System.out.println(solution(3, new int[][]{{1, 1, 0}, {1, 1, 0}, {0, 0, 1}}));
@@ -8,39 +6,23 @@ public class Network {
     private static int solution(int n, int[][] computers) {
         int answer = 0;
 
-        Map<Integer, ArrayList<Integer>> network = new HashMap<>();
-        for (int i = 0; i < computers.length; i++) {
-            for (int j = 0; j < computers.length; j++) {
-                if (computers[i][j] == 1){
-                    ArrayList<Integer> connectedComputers = network.getOrDefault(i, new ArrayList<>());
-                    if (i != j) {
-                        connectedComputers.add(j);
-                    }
-                    network.put(i, connectedComputers);
-                }
-            }
-        }
-
-        boolean[] visited = new boolean[n];
-
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) { // 아직 방문하지 않은 경우
-                Deque<Integer> deque = new ArrayDeque<>();
-                deque.addLast(i);
-
-                while (!deque.isEmpty()) {
-                    int current = deque.pollFirst();
-
-                    if (!visited[current]) {
-                        visited[current] = true;
-                        network.get(current).forEach(deque::addLast);
-                    }
-                }
-
+        boolean[] visited = new boolean[n]; // 연결된 컴퓨터를 확인했는지 체크
+        for (int i = 0; i < visited.length; i++) {
+            if (!visited[i]) {  // 아직 해당 컴퓨터를 확인하지 않은 경우
+                dfs(computers, visited, i);  // 기준 컴퓨터와 연결된 컴퓨터를 모두 확인
                 answer++;
             }
         }
 
         return answer;
+    }
+
+    private static void dfs(int[][] computers, boolean[] visited, int start) {
+        visited[start] = true;  // 기준 컴퓨터를 확인한 것으로 처리하고
+        for (int i = 0; i < computers[start].length; i++) {
+            if (computers[start][i] == 1 && !visited[i]) {  // 기준 컴퓨터와 연결되어 있지만 아직 확인하지 않은 경우
+                dfs(computers, visited, i);  // 다시 다음 컴퓨터를 기준으로 연결된 컴퓨터 모두 확인
+            }
+        }
     }
 }
