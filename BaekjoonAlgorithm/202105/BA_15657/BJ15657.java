b0d383fc6bc6364557bfdf7599a779973fff0c45
hanbee <sonhanbi1002@gmail.com>
2021-05-08 10:43:26 +0900

    중복 조합 구하기

---
+++ BaekjoonAlgorithm/202105/BA_15657/BJ15657.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BJ15657 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
+
+        int[] nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(nums);
+
+        combination(nums, new int[n], n, m, 0, 0);
+    }
+
+    private static void combination(int[] nums, int[] selected, int n, int m, int start, int k) {
+        if (k == m) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < m; i++) {
+                sb.append(selected[i]).append(" ");
+            }
+            System.out.println(sb);
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            selected[k] = nums[i];
+            combination(nums, selected, n, m, i, k + 1);
+        }
+    }
+}
