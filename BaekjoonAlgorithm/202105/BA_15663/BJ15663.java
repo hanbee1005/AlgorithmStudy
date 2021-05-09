a27b0821efa3b17c2294fecc23b896557197a76b
hanbee <sonhanbi1002@gmail.com>
2021-05-10 05:57:06 +0900

    일반 수열 중복 제거 구현

---
+++ BaekjoonAlgorithm/202105/BA_15663/BJ15663.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class BJ15663 {
+    private static Set<String> answer = new HashSet<>();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
+
+        int[] nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(nums);
+
+        permutation(nums, new int[n], new boolean[n], n, m, 0);
+    }
+
+    private static void permutation(int[] nums, int[] selected, boolean[] isSelect, int n, int m, int depth) {
+        if (depth == m) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < depth; i++) {
+                sb.append(selected[i]).append(" ");
+            }
+
+            if (!answer.contains(sb.toString())) {
+                answer.add(sb.toString());
+                System.out.println(sb);
+            }
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!isSelect[i]) {
+                isSelect[i] = true;
+                selected[depth] = nums[i];
+                permutation(nums, selected, isSelect, n, m, depth + 1);
+                isSelect[i] = false;
+            }
+        }
+    }
+}
