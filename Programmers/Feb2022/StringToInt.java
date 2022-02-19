46db2d036d910661ad225db56da14f4bf6ea2afe
hanbee <sonhanbi1002@gmail.com>
2022-02-19 12:20:13 +0900

    문자열을 정수로 바꾸기

---
+++ Programmers/Feb2022/StringToInt.java
@@ -0,0 +1,12 @@
+package Feb2022;
+
+public class StringToInt {
+    public static void main(String[] args) {
+        System.out.println(solution("-1234"));
+    }
+
+    private static int solution(String s) {
+        int answer = Integer.parseInt(s);
+        return answer;
+    }
+}
