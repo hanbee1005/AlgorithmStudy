63174f15030811daea9ed9118f4028a29d9ad7d1
hanbee <sonhanbi1002@gmail.com>
2022-10-31 06:42:04 +0900

    우선순위 큐 최소, 최대 힙

---
+++ BaekjoonAlgorithm/202210/Q11279.java
@@ -0,0 +1,26 @@
+package Oct2022;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Q11279 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((o1, o2) -> o2 - o1);
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (0 == x) {
+                if (minHeap.isEmpty()) System.out.println("0");
+                else System.out.println(minHeap.poll());
+            } else {
+                minHeap.add(x);
+            }
+        }
+    }
+}
