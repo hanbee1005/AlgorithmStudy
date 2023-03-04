fbfe9192e2826d7886b88578a8be31ca78e34f4a
손한비 (Hanbee Son) <37995243+hanbee1005@users.noreply.github.com>
2023-03-05 00:41:59 +0900

    Create BestTimeToBuyAndSellStock_121.java

---
+++ Leetcode/202303/BestTimeToBuyAndSellStock_121.java
@@ -0,0 +1,22 @@
+public class BestTimeToBuyAndSellStock_121 {
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7,1,5,3,6,4}));
+        System.out.println(maxProfit(new int[]{7,6,4,3,1}));
+        System.out.println(maxProfit(new int[]{2,4,1}));
+    }
+
+    private static int maxProfit(int[] prices) {
+        int buy = 10001;
+        int max = 0;
+
+        for (int price : prices) {
+            if (price < buy) {
+                buy = price;
+            }
+
+            max = Math.max(max, price - buy);
+        }
+
+        return max;
+    }
+}
