9edd08726fc15110d6b8254646229f5309f8ea39
hanbee <sonhanbi1002@gmail.com>
2023-09-19 07:14:50 +0900

    top coder 문제 풀이중... 시간제한

--- Programmers/Sep2023/TopCoderBinaryFlips.java
+++ Programmers/Sep2023/TopCoderBinaryFlips.java
@@ -1,5 +1,9 @@
 package Sep2023;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Top Coder BinaryFlips SRM443 Div 1 Level 2
  */
@@ -14,15 +18,43 @@ public class TopCoderBinaryFlips {
         System.out.println(minimalMoves(4, 44, 50));
     }
 
-    // 수학적인 풀이 방법
     private static int minimalMoves(int A, int B, int K) {
-        for (long i = 0; i < A + B; i++) {
-            long rest = i * K - A;
-            long use = ((i / 2) * B + ((i - 1) / 2) * A) * 2;
-            if (rest >= 0 && rest % 2 == 0 && rest <= use) {
-                return (int) i;
+        if (A == 0) return 0;  // 움직일 필요가 없는 경우
+        if (A + B < K) return -1;  // 뒤집을 수 없는 경우
+
+        int[] array = new int[A + B + 1];
+        Arrays.fill(array, -1);
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(A);
+        array[A] = 0;
+
+        while (!q.isEmpty()) {
+            int i = q.poll();  // i는 0의 수
+
+            // j는 0을 뒤집는 수
+            for (int j = Math.max(0, K - (A + B + i)); j < Math.min(i, K); j++) {
+                int nextzero = i + (K - 2 * j); // 다음 상태의 0의 수
+                if (array[nextzero] == -1) { // 처음 도착하는 곳이라면 다음 탐색에 추가합니다.
+                    if (nextzero == 0) return array[i] + 1;  // 답을 찾으면 이것을 리턴합니다.
+                    array[nextzero] = array[i] + 1;
+                    q.add(nextzero);
+                }
             }
         }
-            return -1;
+
+        return -1;
     }
+
+    // 수학적인 풀이 방법
+//    private static int minimalMoves(int A, int B, int K) {
+//        for (long i = 0; i < A + B; i++) {
+//            long rest = i * K - A;
+//            long use = ((i / 2) * B + ((i - 1) / 2) * A) * 2;
+//            if (rest >= 0 && rest % 2 == 0 && rest <= use) {
+//                return (int) i;
+//            }
+//        }
+//            return -1;
+//    }
 }
