adad30b8e0b3e3714412e2913134ffae41fa8eb8
hanbee <sonhanbi1002@gmail.com>
2023-09-19 07:49:21 +0900

    top coder 문제 풀이 level 3. 바이너리 플립

--- Programmers/Sep2023/TopCoderBinaryFlips.java
+++ Programmers/Sep2023/TopCoderBinaryFlips.java
@@ -1,16 +1,9 @@
 package Sep2023;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-
 /**
  * Top Coder BinaryFlips SRM443 Div 1 Level 2
  */
 public class TopCoderBinaryFlips {
-    private static int[] array;
-    private static int k;
-    private static Queue<Integer> q;
 
     public static void main(String[] args) {
         System.out.println(minimalMoves(3, 0, 3));
@@ -26,34 +19,35 @@ public class TopCoderBinaryFlips {
         if (A == 0) return 0;  // 움직일 필요가 없는 경우
         if (A + B < K) return -1;  // 뒤집을 수 없는 경우
 
-        array = new int[A + B + 1];
-        Arrays.fill(array, -1);
+        int minvalue = A, maxvalue = A;
 
-        k = K;
+        // 0의 장수의 현재 최대치와 최소치를 저장합니다.
+        // 짝수인지 홀수인지 밖에 없다는 것에 주의
+        for (int i = 0; i < A + B; i++) {
+            int nextminvalue, nextmaxvalue;
 
-        q = new LinkedList<>();
-        q.add(A);
-        array[A] = 0;
+            // 일단 가능한 0을 줄입니다. = 1로 많이 뒤집습니다.
+            if (minvalue <= K && maxvalue >= K) {
+                // 최대치와 최소치 사이에 K가 있는 경우
+                if (minvalue % 2 == K % 2) return i + 1; // 홀수라면 모두 1이 됩니다.
+                else nextminvalue = 1; // 홀수가 아니라면 0이 1장 남습니다.
+            } else {
+                nextminvalue = Math.min(Math.abs(K - minvalue), Math.abs(K - maxvalue));
+            }
 
-        while (!q.isEmpty()) {
-            int i = q.poll();  // i는 0의 수
+            // 마찬가지로 최대값도 생각합니다.
+            if (minvalue <= (A + B - K) && maxvalue >= (A + B - K)) {
+                if ((A + B - maxvalue) % 2 == K % 2) nextmaxvalue = A + B;
+                else nextmaxvalue = A + B - 1;
+            } else {
+                nextmaxvalue = A + B - Math.min(Math.abs((A + B - K) - minvalue), Math.abs((A + B - K) - maxvalue));
+            }
 
-            check(i, Math.max(0, K - (A + B - i)));  // 0을 최대한 증가시킵니다.
-            check(i, Math.min(i, K));  // 1을 최대한 증가시킵니다.
-            check(i, Math.min(Math.max((i + 1) / 2, K - ((A + B) - i)), K));  // K에 위부터 접근합니다.
-            check(i, Math.min(Math.max(i / 2, K - ((A + B) - i)), K));  // K에 아래부터 접근합니다.
+            minvalue =nextminvalue;
+            maxvalue = nextmaxvalue;
         }
 
-        return array[0];
-    }
-
-    private static void check(int nowzero, int usezero) {
-        // 가능한 0을 증가시킵니다. = 최대한 1을 많이 뒤집습니다.
-        int nextzero = nowzero + k - 2 * usezero;
-        if (array[nextzero] == -1) { // 처음 도착하는 곳이라면 다음 탐색에 추가합니다.
-            array[nextzero] = array[nowzero] + 1;
-            q.add(nextzero);
-        }
+        return -1;
     }
 
     // 수학적인 풀이 방법
