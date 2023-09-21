0e2131d8e22e66844988eecd55592639abe54a71
hanbee <sonhanbi1002@gmail.com>
2023-09-22 07:31:56 +0900

    top coder 문제 풀이 level 2. 칸토어 먼지 (조회 성능 개선)

--- Programmers/Sep2023/TopCoderCantorDust.java
+++ Programmers/Sep2023/TopCoderCantorDust.java
@@ -14,22 +14,45 @@ public class TopCoderCantorDust {
     }
 
     private static int occurrencesNumber(String[] pattern, int time) {
-        int answer = 0;
-
         String fractal = makeFractal(time);
+        int fLength = fractal.length();
 
+        // 가로 세로 길이
         int yLength = pattern[0].length();
         int xLength = pattern.length;
 
-        for (int x = 0; x <= fractal.length() - xLength; x++) {
-            for (int y = 0; y <= fractal.length() - yLength; y++) {
-                if (checkMatch(x, y, fractal, pattern)) {
-                    answer++;
+        // 각 행과 열에 X를 포함할지 여부
+        boolean[] xArr = new boolean[xLength], yArr = new boolean[yLength];
+
+        // 각 행과 열이 'X'를 포함하고 있는지 여부
+        boolean flag = false;
+
+        int x, y;
+
+        for (x = 0; x < xLength; x++) {
+            for (y = 0; y < yLength; y++) {
+                if (pattern[x].charAt(y) == 'X') {
+                    xArr[x] = yArr[y] = flag = true;  // 모순이 있으면 포함하지 않는 것이므로 0을 리턴
                 }
             }
         }
 
-        return answer;
+        for (x = 0; x < xLength; x++) {
+            for (y = 0; y < yLength; y++) {
+                if ((pattern[x].charAt(y) == 'X') != (xArr[x] && yArr[y])) {
+                    return 0;
+                }
+            }
+        }
+
+        // 가로의 일치 수 확인
+        int p = checkMatch(xLength, fLength, fractal, xArr);
+
+        // 세로의 일치 수 확인
+        int q = checkMatch(yLength, fLength, fractal, yArr);
+
+        if (flag) return (p * q);  // 'X'를 포함하는 경우
+        else return (p * (fLength - yLength + 1) + (fLength - xLength + 1) * q - p * q);  // 'X'를 포함하지 않는 경우
     }
 
     private static String makeFractal(int i) {
@@ -42,16 +65,17 @@ public class TopCoderCantorDust {
         return f + empty + f;
     }
 
-    private static boolean checkMatch(int sx, int sy, String fractal, String[] pattern) {
-        for (int i = 0; i < pattern.length; i++) {
-            for (int j = 0; j < pattern[i].length(); j++) {
-                char c = fractal.charAt(sx + i) == 'X' && fractal.charAt(sy + j) == 'X' ? 'X' : '.';
-                if (pattern[i].charAt(j) != c) {
-                    return false;
-                }
+    private static int checkMatch(int M, int L, String fractal, boolean[] arr) {
+        int p = 0;
+
+        for (int k = 0; k + M <= L; ++k) {
+            int x;
+            for (x = 0; x < M; ++x) {
+                if ((fractal.charAt(k + x) == 'X') != arr[x]) break;
             }
+            if (x == M) ++p;
         }
 
-        return true;
+        return p;
     }
 }
