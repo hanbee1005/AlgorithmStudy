6d2689329143bc5b6da7b5dd719b5e52deba68b3
hanbee <sonhanbi1002@gmail.com>
2023-09-26 07:58:34 +0900

    top coder 문제 풀이 level 2. Not Two

--- Programmers/Sep2023/TopCoderNotTwo.java
+++ Programmers/Sep2023/TopCoderNotTwo.java
@@ -4,6 +4,7 @@ package Sep2023;
  * Top Coder Not Two SRM452 Div 1 Level 1
  */
 public class TopCoderNotTwo {
+
     public static void main(String[] args) {
         System.out.println(maxStones(3, 2));
         System.out.println(maxStones(3, 3));
@@ -12,8 +13,13 @@ public class TopCoderNotTwo {
     }
 
     private static int maxStones(int width, int height) {
-        int answer = 0;
+        int i, j, res = 0;
+        for (i = 0; i < 2; i++) {
+            for (j = 0; j < 2; j++) {
+                res += (((width + i) / 2) * ((height + j) / 2) + 1) / 2;
+            }
+        }
 
-        return answer;
+        return res;
     }
 }
