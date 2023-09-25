20a0523d77321fd7a4f30eb2d2c5b9036b879b02
hanbee <sonhanbi1002@gmail.com>
2023-09-25 23:36:28 +0900

    top coder 문제 풀이중...

---
+++ Programmers/Sep2023/TopCoderNotTwo.java
@@ -0,0 +1,19 @@
+package Sep2023;
+
+/**
+ * Top Coder Not Two SRM452 Div 1 Level 1
+ */
+public class TopCoderNotTwo {
+    public static void main(String[] args) {
+        System.out.println(maxStones(3, 2));
+        System.out.println(maxStones(3, 3));
+        System.out.println(maxStones(8, 5));
+
+    }
+
+    private static int maxStones(int width, int height) {
+        int answer = 0;
+
+        return answer;
+    }
+}
