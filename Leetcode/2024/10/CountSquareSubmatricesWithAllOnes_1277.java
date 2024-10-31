02ba435fd09a6d4810120863f5eb66861b953d79
hanbee <sonhanbi1002@gmail.com>
2024-10-31 21:21:42 +0900

    medium_count_square_submatrices_with_all_ones 문제 풀이

---
+++ Leetcode/2024/10/CountSquareSubmatricesWithAllOnes_1277.java
@@ -0,0 +1,30 @@
+package dynamicprogramming;
+
+public class CountSquareSubmatricesWithAllOnes_1277 {
+    public static void main(String[] args) {
+        System.out.println(countSquares(new int[][]{{0,1,1,1},{1,1,1,1},{0,1,1,1}}));
+        System.out.println(countSquares(new int[][]{{1,0,1},{1,1,0},{1,1,0}}));
+    }
+
+    private static int countSquares(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        int totalSquares = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 1) {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;  // 가장자리 초기화
+                    } else {
+                        dp[i][j] = Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1])) + 1;
+                    }
+                    totalSquares += dp[i][j];
+                }
+            }
+        }
+
+        return totalSquares;
+    }
+}
