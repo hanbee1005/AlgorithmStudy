86e97545099e685126b71ad2aa18b5d52422fe19
hanbee <sonhanbi1002@gmail.com>
2025-06-04 22:11:58 +0900

    medium_3403_find_the_lexicographically_largest_string_from_the_box_i 문제 풀이

---
+++ Leetcode/2025/06/FindTheLexicographicallyLargestStringFromTheBoxI_3403.java
@@ -0,0 +1,35 @@
+package string;
+
+public class FindTheLexicographicallyLargestStringFromTheBoxI_3403 {
+    public static void main(String[] args) {
+        System.out.println(answerString("dbca", 2));
+        System.out.println(answerString("gggg", 4));
+    }
+
+    private static String lastSubstring(String s) {
+        int i = 0, j = 1, n = s.length();
+        while (j < n) {
+            int k = 0;
+            while (j + k < n && s.charAt(i + k) == s.charAt(j + k)) {
+                k++;
+            }
+            if (j + k < n && s.charAt(i + k) < s.charAt(j + k)) {
+                int t = i;
+                i = j;
+                j = Math.max(j + 1, t + k + 1);
+            } else {
+                j = j + k + 1;
+            }
+        }
+        return s.substring(i);
+    }
+
+    private static String answerString(String word, int numFriends) {
+        if (numFriends == 1) {
+            return word;
+        }
+        String last = lastSubstring(word);
+        int n = word.length(), m = last.length();
+        return last.substring(0, Math.min(m, n - numFriends + 1));
+    }
+}
